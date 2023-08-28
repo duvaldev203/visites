11bfbef04a3d217c9b72e28aff6293a8f168f156
duvaldev203 <74819278+duvaldev203@users.noreply.github.com>
2023-08-28 09:54:56 +0100

    Mail service integrated

--- src/backend/spring-boot/src/main/java/com/example/visites/Application.java
+++ src/backend/spring-boot/src/main/java/com/example/visites/Application.java
@@ -1,8 +1,6 @@
 package com.example.visites;
 
-import com.example.visites.repositories.RoleRepository;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
